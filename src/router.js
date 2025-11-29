07c3f377514c9686994ee623f97dcd05452fa9ec
Leslie Esquivel <eleslie14@outlook.com>
2025-11-29 09:50:02 -0600

    modified vite config and router.js

--- src/router.js
+++ src/router.js
@@ -4,18 +4,18 @@ import HomePage from '@/views/HomePage.vue'
 import CardDetails from './views/CardDetails.vue'
 import LoginPage from './views/LoginPage.vue'
 import SettingsPage from './views/SettingsPage.vue'
-//import { useAuth } from '@vueuse/firebase'
-//import { useAttrs } from 'vue'
+import { useAuth } from '@vueuse/firebase'
+import { useAttrs } from 'vue'
 
 import { useAuth } from './composables/useAuth'
 const { isAuthenticated } = useAuth()
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
-  { path: '/employees/:id', name: 'CardDetails', component: CardDetails },
-  { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/settings', name: 'SettingsPage', component: SettingsPage, meta: { requiresAuth: true } },
+  { path: '/company-directory-api/', name: 'Home', component: HomePage },
+  { path: '/company-directory-api/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/company-directory-api/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/company-directory-api/login', name: 'LoginPage', component: LoginPage },
+  { path: '/company-directory-api/settings', name: 'SettingsPage', component: SettingsPage, meta: { requiresAuth: true } },
 ]
 
 const router = createRouter({
